35d4e0c3d672da31a41439967e5b81e4f5db0ba3
olenai <helen.aizman@gmail.com>
2024-09-12 15:54:01 +0300

    second release

--- olena_aiz/settings.py
+++ olena_aiz/settings.py
@@ -32,7 +32,7 @@ DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 
 INSTALLED_APPS = [
