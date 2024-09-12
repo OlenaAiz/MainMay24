5ed9d5cd51218defe10458f971c32ee935091e20
olenai <helen.aizman@gmail.com>
2024-09-12 15:45:57 +0300

    second release

--- olena_aiz/settings.py
+++ olena_aiz/settings.py
@@ -30,8 +30,8 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DATABASE_URL')
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DB_PORT = os.environ.get('DB_PORT')
+DATABASE_URL = os.environ.get('DB_URL')
 DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 
